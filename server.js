56f04cc7ee4a5398975204e15cfe7c336df7fe6b
vosem <trefoil2@ukr.net>
2018-12-22 12:02:14 +0200

    fixing

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ var express = require('express'),
 	bodyParser = require('body-parser'),
 	mongodb = require('mongodb'),
 	assert = require('assert');
-	// var url = "mongodb://heroku_nvq9ztl7:8k7mm8jmofmr5m9r0etp429stk@ds139534.mlab.com:39534/heroku_nvq9ztl7";
 
 var app = express();
 var db; // Create a database variable outside of the database connection callback to reuse the connection pool in app
@@ -16,9 +15,8 @@ var data;
 
 app.post('/submit', function(req, res) {
 	data = req.body;
-	mongodb.MongoClient.connect(process.env.MONGODB_URI, { useNewUrlParser: true }, function (err, client) {
+	mongodb.MongoClient.connect(process.env.MONGOLAB_URI, { useNewUrlParser: true }, function (err, client) {
 		assert.equal(null, err);
-	    console.log('Connection established to', url);
 		db = client.db();
 	    saveData(db, function(response){
 	    	client.close();
@@ -45,9 +43,8 @@ var saveData = function(db, callback) {
 
 app.post('/load', function(req, res) {
 	data = req.body;
-	mongodb.MongoClient.connect(process.env.MONGODB_URI, { useNewUrlParser: true }, function (err, client) {
+	mongodb.MongoClient.connect(process.env.MONGOLAB_URI, { useNewUrlParser: true }, function (err, client) {
 		assert.equal(null, err);
-	    console.log('Connection established to', url);
 		db = client.db();
 	    getData(db, function(response){
 	    	client.close();
@@ -72,7 +69,6 @@ var getData = function(db, callback) {
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/index.html'));
-  console.log(url);
 });
 
 app.listen(process.env.PORT || 8080);
